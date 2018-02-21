f30e65072bd2897517cf7d62479e106fbd4aac74
kiwi <rajanpau@usc.edu>
2018-02-20 17:26:01 -0800

    more indents

--- ee250-lab04/part3/ultrasonicClient.py
+++ ee250-lab04/part3/ultrasonicClient.py
@@ -1,30 +1,16 @@
-# Ultrasonic Sensor Client
-#
-# This code runs on the Raspberry Pi. It should sit in a loop which reads from
-# the Grove Ultrasonic Ranger and sends the reading to the Ultrasonic Sensor
-# Server running on your VM via UDP packets.
-
-import sys
-# By appending the folder of all the GrovePi libraries to the system path here,
-# we are able to successfully `import grovepi`
-sys.path.append('../../Software/Python/')
-
-import grovepi
-
 import socket
+import grovepi
+import sys
+sys.path.append('../../Software/Python')
 
 def Main():
-
-	# Connect the Grove Ultrasonic Ranger to digital port D4
-	# SIG,NC,VCC,GND
-	ultrasonic_ranger = 3
-
-	# Change the host and port as needed. For ports, use a number in the 9000
+    # Change the host and port as needed. For ports, use a number in the 9000
     # range.
-    host = '192.168.1.169'
-    port = 1024
+	ultrasonic_ranger = 3
+    host = '127.0.0.1'
+    port = 1023
 
-    server_addr = '192.168.1.206'
+    server_addr = '127.0.0.1'
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((host,port))
@@ -32,20 +18,18 @@ def Main():
     # UDP is connectionless, so a client does not formally connect to a server
     # before sending a message.
     dst_port = input("destination port-> ")
-
+    message = input("message-> ")
     while message != 'q':
+
+		try:
+			message = grovepi.ultrasonicRead(ultrasonic_ranger)
+		except TypeError:
+			message = "Error"
+		except IOError:
+			message = "Error"
+
         #tuples are immutable so we need to overwrite the last tuple
         server = (server_addr, int(dst_port))
-
-        while True:
-			try:
-				# Read distance value from Ultrasonic
-				message = (grovepi.ultrasonicRead(ultrasonic_ranger))
-
-			except TypeError:
-				message = ("Error")
-			except IOError:
-				message = ("Error")
 
         # for UDP, sendto() and recvfrom() are used instead
         s.sendto(message.encode('utf-8'), server)
