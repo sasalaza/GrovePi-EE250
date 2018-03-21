e61302fd4e844f7662e73f6913e6d7a77295948b
kiwi <rajanpau@usc.edu>
2018-03-20 18:39:43 -0700

    spacing

--- ee250/lab07/rpi_pub_and_sub.py
+++ ee250/lab07/rpi_pub_and_sub.py
@@ -18,32 +18,30 @@ def custom_callback(client, userdata, message):
 		digitalWrite(led_pin, 0)
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
+	print("Connected to server (i.e., broker) with result code "+str(rc))
 
-    #subscribe to topics of interest here
+	#subscribe to topics of interest here
 	client.subscribe("anrg-pi3/led")
 	client.message_callback_add("anrg-pi3/led", custom_callback)
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
-    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+	print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 if __name__ == '__main__':
-    #this section is covered in publisher_and_subscriber_example.py
-    client = mqtt.Client()
-    client.on_message = on_message
-    client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-    client.loop_start()
+	#this section is covered in publisher_and_subscriber_example.py
+	client = mqtt.Client()
+	client.on_message = on_message
+	client.on_connect = on_connect
+	client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+	client.loop_start()
 
-    while True:
-        try:
+	while True:
+		try:
 		message = str(grovepi.ultrasonicRead(ultrasonic_pin))
 	except TypeError:
 		message = "TypeError"
 	except IOError:
 		message = "IOError"
 	client.publish("anrg-pi3/ultrasonicRanger", message)
-        time.sleep(1)
-            
-
+		time.sleep(1)
--- ee250/lab07/vm_subscriber.py
+++ ee250/lab07/vm_subscriber.py
@@ -9,25 +9,23 @@ def custom_callback(client, userdata, message):
 	print("custom_callback: " + message.topic + " " + "\"" + str(message.payload, "utf-8") + "\"")
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
-	
-    client.subscribe("anrg-pi3/ultrasonicRanger")
-    client.message_callback_add("anrg-pi3/ultrasonicRanger", custom_callback)
-    #subscribe to the ultrasonic ranger topic here
+	print("Connected to server (i.e., broker) with result code "+str(rc))
+
+	client.subscribe("anrg-pi3/ultrasonicRanger")
+	client.message_callback_add("anrg-pi3/ultrasonicRanger", custom_callback)
+	#subscribe to the ultrasonic ranger topic here
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
-    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+	print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 if __name__ == '__main__':
-    #this section is covered in publisher_and_subscriber_example.py
-    client = mqtt.Client()
-    client.on_message = on_message
-    client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-    client.loop_forever()
+	#this section is covered in publisher_and_subscriber_example.py
+	client = mqtt.Client()
+	client.on_message = on_message
+	client.on_connect = on_connect
+	client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+	client.loop_forever()
 
-    while True: 
-        time.sleep(1)
-            
-
+	while True:
+		time.sleep(1)
