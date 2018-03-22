1cc7491fe186c22fdf63a0acf11325ccc414fbe2
rajanpau <36213633+rajanpau@users.noreply.github.com>
2018-03-21 22:27:54 -0700

    Update rpi_pub_and_sub.py

--- ee250/lab07/rpi_pub_and_sub.py
+++ ee250/lab07/rpi_pub_and_sub.py
@@ -16,9 +16,9 @@ led_pin = 2
 def custom_callback(client, userdata, message):
 	print("custom_callback: " + message.topic + " " + "\"" + str(message.payload, "utf-8") + "\"")
 
-	if message.payload == "LED_ON":
+	if str(message.payload) == "LED_ON":
 		digitalWrite(led_pin, 1)
-	elif message.payload == "LED_OFF":
+	elif str(message.payload) == "LED_OFF":
 		digitalWrite(led_pin, 0)
 
 def on_connect(client, userdata, flags, rc):
