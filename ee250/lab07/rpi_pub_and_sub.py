5d7871da7db57553e0d8d1b0a40f1eff247332c0
wukong <rajanpau@usc.edu>
2018-03-23 15:32:15 -0700

    led

--- ee250/lab07/rpi_pub_and_sub.py
+++ ee250/lab07/rpi_pub_and_sub.py
@@ -17,6 +17,7 @@ pinMode(led_pin, "OUTPUT")
 
 def custom_callback(client, userdata, message):
 	print("custom_callback: " + message.topic + " " + "\"" + str(message.payload, "utf-8") + "\"")
+	print(str(message.payload, "utf-8"))
 	if str(message.payload) == "b\'LED_ON\'":
 		print("on")
 		#digitalWrite(led_pin, "HIGH")
