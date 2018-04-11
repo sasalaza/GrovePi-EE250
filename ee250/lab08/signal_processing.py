dd1a3f3761927e2e4228a88846d68fef58c02e50
bmorkp <rajanpau@usc.edu>
2018-04-10 17:33:55 -0700

    working movement!

--- ee250/lab08/signal_processing.py
+++ ee250/lab08/signal_processing.py
@@ -67,24 +67,26 @@ if __name__ == '__main__':
 	    'time': str(datetime.now()),
 	    'event': "None"
 	}
-	time.sleep(2)
-	"""
+
+	string1 = ""
+	string2 = ""
+	time.sleep(4)
+
 	set1 = ranger1_dist[-10:]
 	set2 = ranger2_dist[-10:]
 	sum1 = 0
 	sum2 = 0
-	for i in range(0,len(temp1)):
-		if temp1[i] >= 125:
-			temp1[i]=125
-		if temp2[i] >= 125:
-			temp2[i]=125
-		sum1 = sum1 + temp1[i]
-		sum2 = sum2 + temp2[i]
+	for i in range(0,len(set1)):
+		if set1[i] >= 125:
+			set1[i]=125
+		if set2[i] >= 125:
+			set2[i]=125
+		sum1 = sum1 + set1[i]
+		sum2 = sum2 + set2[i]
 	threshold1 = sum1/SUB_LIST_LENGTH
 	threshold2 = sum2/SUB_LIST_LENGTH
 	print("Threshold1: " + str(threshold1))
 	print("Threshold2: " + str(threshold2))
-	"""
 	
 	while True:
 		""" You have two lists, ranger1_dist and ranger2_dist, which hold a window
@@ -120,14 +122,50 @@ if __name__ == '__main__':
 		sum2 = sum2/SUB_LIST_LENGTH
 
 		#print(str(sum1)+","+str(sum2))
-		
+			
 		average1.append(sum1)
 		average2.append(sum2)
+		difference1.append(int(threshold1-sum1))
+		difference2.append(int(threshold2-sum2))
 		sum1 = 0
 		sum2 = 0
 		average1 = average1[-SUB_LIST_LENGTH:]
 		average2 = average2[-SUB_LIST_LENGTH:]
-
+		difference1 = difference1[-SUB_LIST_LENGTH:]
+		difference2 = difference2[-SUB_LIST_LENGTH:]
+		#print(difference1)
+		#print(difference2)
+	
+		for i in range(0,len(difference1)-1):
+			if difference1[i] > 20 and difference1[i]>sum1:
+				sum1 = difference1[i]
+			if difference2[i] > 20 and difference2[i]>sum2:
+				sum2 = difference2[i]
+		if difference1[-1] <= 20 and sum1 > 20:
+			if string1 == "":
+				string1 = "sensor1"
+			elif string2 == "" and string1 != "sensor1":
+				if string2 == "":
+					string2 = "sensor1"
+		if difference2[-1] <= 20 and sum2 > 20:
+			if string1 == "":
+				string1 = "sensor2"
+			elif string2 == "" and string1 != "sensor2":
+				if string2 == "":
+					string2 = "sensor2"
+				
+		if string1 != "" and string2 !="":
+			if string1 == "sensor1":
+				tempstring = "Moving Left"
+			elif string1 == "sensor2":
+				tempstring = "Moving Right"
+			string1 = ""
+			string2 = ""
+			difference1[:] = []
+			difference2[:] = []
+		print(tempstring)
+		
+		"""
 		sum1 = (average1[len(average1)-1]-average1[0])/(SUB_LIST_LENGTH-1)
 		sum2 = (average2[len(average2)-1]-average2[0])/(SUB_LIST_LENGTH-1)
 
@@ -149,15 +187,16 @@ if __name__ == '__main__':
 				tempstring = "Moving Left"
 			difference1[:] = []
 			difference2[:] = []
-
+		"""
 		#print(average1)
 		#print(average2)
+		"""
 		if tempstring != "":
-			payload['event'] = "Average"
+			payload['event'] = tempstring
 			response = requests.post("http://0.0.0.0:5000/post-event", headers = hdr, data = json.dumps(payload))
 			print(response.json())
+		"""	
 		"""
-		
 		#sum1 = average1[len(average1)-1]-average1[0]
 		#sum2 = average2[len(average2)-1]-average2[0]
 		#print(str(sum1)+","+str(sum2))
